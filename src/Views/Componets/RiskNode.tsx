f648c8ed009601edfbfb6c407e96903f48be6a28
Coaixy <Coaixy@outlook.com>
2024-12-17 10:54:27 +0800

    rewrite

--- src/Views/Componets/RiskNode.tsx
+++
@@ -1,9 +0,0 @@
-import Sword from '../Assets/Sword.png'
-const RiskNode = (props: { x: number, y: number ,risk:string}) => {
-    return (<div style={{ position: "fixed", left: props.x, top: props.y }} className='text-center'>
-        <img src={Sword} alt="Risk Node" />
-        <p className='underline font-bold'>{props.risk}</p>
-    </div>);
-}
-
-export default RiskNode;
